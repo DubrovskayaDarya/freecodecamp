eb252ec107bc4170331ba7274e2cf862c8589afa
Darya.Dubrovskaya <dashadubrovskaya1999@gmail.com>
2022-03-14 23:39:12 +0300

    new JS files

---
+++ js_4_const.js
@@ -0,0 +1,5 @@
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome!";
+console.log(FCC, fact);
+//const has all the awesome features that let has, with the added bonus that variables declared using const are read-only.
