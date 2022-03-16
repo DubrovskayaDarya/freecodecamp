df4da07766f8ddffdf866cb4e14742b80d15f6f9
Darya.Dubrovskaya <dashadubrovskaya1999@gmail.com>
2022-03-16 14:31:39 +0300

    js - using double or single quotes

---
+++ js_quotes.js
@@ -0,0 +1,3 @@
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+//String values in JavaScript may be written with single or double quotes, as long as you start and end with the same type of quote.
+// Unlike some other programming languages, single and double quotes work the same in JavaScript.
